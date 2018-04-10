b8513bcb342ad7e31bca22a940072d5b78ada8db
zhanglongde <1039326495@qq.com>
2018-04-10 23:09:37 +0800

    修复发布问题

--- src/containers/Container.vue
+++ src/containers/Container.vue
@@ -7,7 +7,7 @@
       </transition>
       <tab-bar slot="footer">
         <tab-bar-item :link="{path: '/Home'}">Home</tab-bar-item>
-        <tab-bar-item :link="{path: '/Demo'}">Demo</tab-bar-item>
+        <tab-bar-item selected :link="{path: '/Demo'}">Demo</tab-bar-item>
         <tab-bar-item :link="{path: '/Me'}">我</tab-bar-item>
       </tab-bar>
     </view-box>
@@ -16,18 +16,6 @@
     <!--</transition>-->
   </div>
 </template>
-<style>
-  html,body, div#app{
-    height: 100%;
-    /*width: 100%;*/
-    /*overflow-y:hidden;*/
-    overflow-x:hidden;
-  }
-  .view-box .z-header{
-    position:absolute;
-    left:0;right:0;
-  }
-</style>
 <script>
   export default {
     name: 'PageContainer'
